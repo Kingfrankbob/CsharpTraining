4705209f85c554e8ff19ab66bbf86312d964c198
Stephen Morgan <smorgan@goldpointsystems.com>
2022-07-06 17:35:42 -0600

    Created NYC_One

--- p4 (arrays)/Program.cs
+++ p4 (arrays)/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace Vacashon
 {
-    class program
-    {
-        public static void Main()
-        {
+  class program
+  {
+    public static void Main(){
+      Console.ForegroundColor = ConsoleColor.Gray;
+      Console.BackgroundColor = ConsoleColor.Black;
+      int choice = getInput.getChoice();
+      switch (choice)
+      {
 
-        }
+        case 1:
+          NYC_ONE.noFriends();
+          program.Main();
+          break;
+
+        // case 2:
+        //   appleTwo.itOkay();
+        //   program.Main();
+        //   break;
+
+        // case 3:
+        //   kiwiThree.kwFactor();
+        //   program.Main();
+        //   break;
+
+        // case 4:
+        //   blueberryFour.randomNumberGuess();
+        //   program.Main();
+        //   break;
+
+        // case 5:
+        //   strawberryFive.MMMck();
+        //   program.Main();
+        //   break;
+
+        // case 6:
+        //   Console.ForegroundColor = ConsoleColor.Green;
+        //   Console.WriteLine("\nGoodbye\n");
+        //   Console.ForegroundColor = ConsoleColor.Gray;
+        //   Console.Beep();
+        //   Console.Beep();
+        //   break;
+
+        default:
+          // Console.WriteLine("Invalid Input");
+          // program.Main();
+          break;
+
+      }
     }
+  }
 }
