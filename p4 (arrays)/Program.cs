585ce4ac1e907b11e13069ebe7a3a207d57dce3b
Kingfrankbob <AJWpcc@gmail.com>
2022-07-06 19:01:38 -0500

    Added new files

--- p4 (arrays)/Program.cs
+++ p4 (arrays)/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace Vacashon
 {
-    class program
-    {
-        public static void Main()
-        {
+  class program
+  {
+    public static void Main(){
+      Console.ForegroundColor = ConsoleColor.Gray;
+      Console.BackgroundColor = ConsoleColor.Black;
+      int choice = int.Parse(Console.ReadLine());
+      switch (choice)
+      {
 
-        }
+        case 1:
+         // NYC_ONE.noFriends();
+          program.Main();
+          break;
+
+         case 2:
+           madagascarTwo.Reversi();
+           program.Main();
+           break;
+
+        // case 3:
+        //   kiwiThree.kwFactor();
+        //   program.Main();
+        //   break;
+
+        // case 4:
+        //   blueberryFour.randomNumberGuess();
+        //   program.Main();
+        //   break;
+
+        // case 5:
+        //   strawberryFive.MMMck();
+        //   program.Main();
+        //   break;
+
+        // case 6:
+        //   Console.ForegroundColor = ConsoleColor.Green;
+        //   Console.WriteLine("\nGoodbye\n");
+        //   Console.ForegroundColor = ConsoleColor.Gray;
+        //   Console.Beep();
+        //   Console.Beep();
+        //   break;
+
+        default:
+          // Console.WriteLine("Invalid Input");
+          // program.Main();
+          break;
+
+      }
     }
+  }
 }
