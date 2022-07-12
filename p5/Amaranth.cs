f25dc5b81e0064a319a7e7cf7d3fa38ba095e740
Kingfrankbob <AJWpcc@gmail.com>
2022-07-12 14:48:30 -0500

    other commits

--- p5/Amaranth.cs
+++ p5/Amaranth.cs
@@ -9,15 +9,17 @@ namespace yarn
     {
         public static void Consornt()
         {
-            try
-            {
+            
+            
             var extraarray = getInput.getnumbyhyphen();
             int cont = 0;
             int length = extraarray.Length;
             for(int i = 0; i < length; i++)
             {
-                if(extraarray[i] == (extraarray[i + 1] - 1)) cont = 1;
-                else cont = 0;
+                if (i == (length - 1)) break;
+                else if(extraarray[i] == (extraarray[i + 1] - 1)) cont = 1;
+                else if(extraarray[i] != (extraarray[i + 1] - 1)) cont = 0;
+                
             }
             if(cont == 1)
             {
