5b33c277d45b03ff1166bab78174c0d9437f2739
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2022-07-12 14:02:30 -0500

    Please allow this (#6) (#7)

---
+++ AmaranthOne.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yarn
+{
+    public class AmaranthOne
+    {
+        public static void Consornt()
+        {
+            
+        }
+        
+    }
+}
