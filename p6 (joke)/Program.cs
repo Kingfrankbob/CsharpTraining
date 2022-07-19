8df52ff0ca3e78b9033f5bf2f4027b10fbde9072
Stephen Morgan <smorgan@goldpointsystems.com>
2022-07-19 13:18:48 -0600

    stuff

--- p6 (joke)/Program.cs
+++ p6 (joke)/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace comedians
+{
+  class Program {
+    static void Main() {
+      
+    }
+  }
+}
---
+++ p6 (joke)/RobinWilliams.cs
@@ -0,0 +1,8 @@
+namespace comedians
+{
+  public static class RobinWilliams{
+    public static void Robin(){
+      //2 - Write a program that reads a text file and displays the longest word in the file.
+    }
+  }
+}
