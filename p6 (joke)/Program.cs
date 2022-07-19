4d5773187da10b7b2c061681bdf4e6f0f6788172
Stephen Morgan <smorgan@goldpointsystems.com>
2022-07-19 13:28:32 -0600

    Problem two

--- p6 (joke)/Program.cs
+++ p6 (joke)/Program.cs
@@ -2,7 +2,7 @@
 {
   class Program {
     static void Main() {
-      
+      RobinWilliams.Reads();
     }
   }
 }
--- p6 (joke)/RobinWilliams.cs
+++ p6 (joke)/RobinWilliams.cs
@@ -1,8 +1,12 @@
 namespace comedians
 {
-  public static class RobinWilliams{
-    public static void Robin(){
-      //2 - Write a program that reads a text file and displays the longest word in the file.
+  public static class RobinWilliams
+  {
+    public static void Reads()
+    {
+      string[] words = File.ReadAllText("BeeMovie.txt").Split(' ');
+      string longestWord = words.OrderByDescending(x => x.Length).First();
+      Console.WriteLine("The longest word in the file is: " + longestWord);
     }
   }
 }
