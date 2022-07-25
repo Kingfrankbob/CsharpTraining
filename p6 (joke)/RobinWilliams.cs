cf7b68822be106adb7dd523586822a1a6e692b9a
Kingfrankbob <AJWpcc@gmail.com>
2022-07-25 14:38:25 -0500

    COMMIT_EDITMSG

--- p6 (joke)/RobinWilliams.cs
+++ p6 (joke)/RobinWilliams.cs
@@ -4,9 +4,10 @@ namespace comedians
   {
     public static void Reads()
     {
-      var words = File.ReadAllText("BeeMovie.txt").Split(' ').OrderByDescending(x => x.Length).First();
-      Console.WriteLine("The longest word in the file is: " + words);
+      Console.WriteLine("The longest word in the file is: {0} ", File.ReadAllText("BeeMovie.txt").Split(' ').OrderByDescending(x => x.Length).First());
     }
   }
 }
+      //var words = File.ReadAllText("BeeMovie.txt").Split(' ').OrderByDescending(x => x.Length).First();
       //string longestWord = words.OrderByDescending(x => x.Length).First(); //Goes in between 7 and 8
+      //Console.WriteLine("The longest word in the file is: " + longestWord)
