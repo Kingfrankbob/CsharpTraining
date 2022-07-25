ea7221102bdd0cb9b00cd306ac7cae0274ad653e
Stephen Morgan <smorgan@goldpointsystems.com>
2022-07-25 13:45:30 -0600

    Program

--- p7 (Classes n' stuff)/Program.cs
+++ p7 (Classes n' stuff)/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace School
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
