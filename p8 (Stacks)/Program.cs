c122fe6a7d123c534e45b8dbc336dbbf5a70e1de
Stephen Morgan <smorgan@goldpointsystems.com>
2022-07-27 15:08:01 -0600

    SStack

--- p8 (Stacks)/Program.cs
+++ p8 (Stacks)/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Garbage
+{ 
+   class program{
+
+    public static void Main(){
+      BigStacks.SMain();
+
+    }
+  }
+}
